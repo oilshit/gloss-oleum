65b9fdbbfb2594a94f45a50745ad65805a36d8dd
daimessdn <daimessdn@gmail.com>
2020-07-25 01:23:51 +0700

    added explanation of glossary.js

--- glossary.js
+++ glossary.js
@@ -1,16 +1,21 @@
+// let's init'd the glossary class
 class Glossary {
+  // define glossary term
   constructor(name) {
     this.name = name;
     this.definitionList = [];
   }
 
+  // add dedinition to the list
   addDefinitionList(definitionList) {
     this.definitionList.push(definitionList);
-
-    console.log(this.definitionList);
+    
+    console.log("one definition added");
   }
 }
 
+// let's init'd the glossary item class
+//// it will be used for appending the Glossary definitionList
 class GlossaryItem {
   // init'd constructor for glossary item
   constructor(definition, author, year, title, publisher) {
@@ -30,8 +35,12 @@ class GlossaryItem {
   }
 }
 
+// let's init the first glossary
 const glossary1 = new Glossary("javascript");
 
+console.log();
+
+// and alse the first glossary item
 const item1 = new GlossaryItem(
   "object orient programming language designed to make web development easier and more attractive",
   "Laura M.",
@@ -40,18 +49,25 @@ const item1 = new GlossaryItem(
   "BitDegree"
 );
 
-// const item2 = new GlossaryItem(
-//   "a high-level, dynamic, untyped, and interpreted programming language. It has been standardized in the ECMAScript language specification",
-//   "Tom McFarlin",
-//   2016,
-//   "What Is JavaScript?",
-//   "tutsplus"
-// );
+// let's see what item1 looks like
+console.log(item1);
 
+console.log();
+
+// let's use information of item1
+//// to get the author based on IPA
 console.log(item1.getAuthor());
+
+console.log();
 
 // console.log(item2.getAuthor());
 
+// let see what glossary1 looks like
+console.log(glossary1);
+
+console.log();
+
+// let's add several definition items to glossary1
 glossary1.addDefinitionList(item1);
 glossary1.addDefinitionList(new GlossaryItem(
   "a high-level, dynamic, untyped, and interpreted programming language. It has been standardized in the ECMAScript language specification",
@@ -61,5 +77,10 @@ glossary1.addDefinitionList(new GlossaryItem(
   "tutsplus"
 ));
 
+// let's see the final of glossary1 after the addition
+console.log(glossary1);
+
+console.log();
+
 // let's try gather information of the class inside the class
-console.log(glossary1.definitionList[0].getAuthor());
+console.log(glossary1.definitionList[1].getAuthor());
