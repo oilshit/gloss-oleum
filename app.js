131975888c6e0353ac1b9ec022c2b0bd90c75dfa
daimessdn <daimessdn@gmail.com>
2020-07-28 10:08:09 +0700

    testing with mongodb

--- app.js
+++ app.js
@@ -16,6 +16,21 @@ mongo.connect(url, {
     return
   }
   console.log("mongodb is here to serve your data. enjoy!");
+
+  const db = client.db('kennel')
+  const collection = db.collection('dogs')
+  collection.insertOne({name: 'Roger'}, (err, result) => {
+    // console.log(result);
+  })
+
+  collection.insertMany([{name: 'Togo'}, {name: 'Syd'}], (err, result) => {
+
+  })
+
+  collection.find().toArray((err, items) => {
+    console.log(items)
+  })
+  
 })
 
 app.get("/", (requset, response) => {
