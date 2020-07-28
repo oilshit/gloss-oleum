f963865237c4cc58de208eb186b374e13dba86f1
daimessdn <daimessdn@gmail.com>
2020-07-28 08:14:12 +0700

    updated note on 27/7/2020

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const mongo = require("mongodb").MongoClient;
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const url = 'mongodb://localhost:27017';
 
 const glossary = require("./glossary.js");
+
+mongo.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, (err, client) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log("mongodb is here to serve your data. enjoy!");
+})
 
 app.get("/", (requset, response) => {
   response.send("Hello, world");
@@ -11,4 +24,4 @@ app.get("/", (requset, response) => {
 
 app.listen(PORT, () => {
   console.log(`listening into http://localhost:${PORT}`);
-}); 
+});
