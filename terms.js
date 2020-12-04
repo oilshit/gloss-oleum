60bad9cfedab4edfefab8685b5fd897c462050b6
daimessdn@gmail.com <daimessdn@gmail.com>
2020-12-04 10:08:56 +0700

    added terms in separated file

---
+++ terms.js
@@ -0,0 +1,32 @@
+const terms = [
+    {
+        term: "liquid specific gravity",
+        definition: "ratio of the density of the liquid to the density of water both taken at the same temperature and pressure.",
+        citation: {
+            author: "William D. McCain Jr.",
+            year: 1990,
+            title: "Properties of Petroleum Fluids",
+            publisher: "Penwell Books"
+        }
+    },
+    {
+        term: "oil formation volume factor",
+        definition: "volume of reservoir oil required to produce one barrel of oil in the stock tank.",
+        citation: {
+            author: "William D. McCain Jr.",
+            year: 1990,
+            title: "Properties of Petroleum Fluids",
+            publisher: "Penwell Books"
+        }
+    },
+    {
+        term: "solution gas-oil ratio (GOR) ",
+        definition: "quantity of gas dissolved in an oil at reservoir conditions.",
+        citation: {
+            author: "William D. McCain Jr.",
+            year: 1990,
+            title: "Properties of Petroleum Fluids",
+            publisher: "Penwell Books"
+        }
+    },
+];
